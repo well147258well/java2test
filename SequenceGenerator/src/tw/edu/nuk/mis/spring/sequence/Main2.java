00ea94e196a4c5405e0cefee6f40f01c659fc107
well147258well <well147258well@172.20.107.94>
2016-11-26 11:40:28 +0800

    asdsf

---
+++ SequenceGenerator/src/tw/edu/nuk/mis/spring/sequence/Main2.java
@@ -0,0 +1,22 @@
+package tw.edu.nuk.mis.spring.sequence;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Sequence-Beans.xml");
+		
+		SequenceGenerator generator = (SequenceGenerator) context.getBean("sequenceGenerator");
+		
+		System.out.println(generator.getSequence());
+		System.out.println(generator.getSequence());
+		System.out.println(generator.getSequence());
+
+	}
+
+}
